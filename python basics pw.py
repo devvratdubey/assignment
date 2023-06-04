88ce57d5ed5aac7259d651937b9b39a8bcd5ee83
devvratdubey <135494312+devvratdubey@users.noreply.github.com>
2023-06-04 11:03:00 +0530

    Add files via upload

---
+++ python basics pw.py
@@ -0,0 +1,1802 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+1+1
+
+
+# In[2]:
+
+
+5*6
+
+
+# In[3]:
+
+
+a=10
+
+
+# In[4]:
+
+
+a
+
+
+# In[5]:
+
+
+sudh=20
+
+
+# In[6]:
+
+
+sudh
+
+
+# In[7]:
+
+
+a+sudh
+
+
+# In[8]:
+
+
+type(a)
+
+
+# In[9]:
+
+
+type(sudh)
+
+
+# In[10]:
+
+
+b=2.09
+
+
+# In[11]:
+
+
+type(b)
+
+
+# In[12]:
+
+
+d="devvrat dubey"
+
+
+# In[13]:
+
+
+type(d)
+
+
+# In[14]:
+
+
+e="this is my firs tech class"
+
+
+# In[15]:
+
+
+e
+
+
+# In[16]:
+
+
+type(e)
+
+
+# In[17]:
+
+
+1>2
+
+
+# In[18]:
+
+
+n=True
+
+
+# In[19]:
+
+
+type(n)
+
+
+# In[20]:
+
+
+m=False
+
+
+# In[21]:
+
+
+type(m)
+
+
+# In[22]:
+
+
+m+n
+
+
+# In[23]:
+
+
+True*True
+
+
+# In[24]:
+
+
+True*False
+
+
+# In[25]:
+
+
+True/False
+
+
+# In[27]:
+
+
+v=5+8j
+
+
+# In[28]:
+
+
+type(v)
+
+
+# In[29]:
+
+
+v.real
+
+
+# In[30]:
+
+
+v.imag
+
+
+# In[31]:
+
+
+sudh=3+2j
+
+
+# In[32]:
+
+
+sudh.real
+
+
+# In[34]:
+
+
+sudh.imag
+
+
+# In[35]:
+
+
+help(type)
+
+
+# In[36]:
+
+
+print("hello world")
+
+
+# In[37]:
+
+
+print("my name is devvrat dubey")
+
+
+# In[40]:
+
+
+print(sudh)
+
+
+# In[42]:
+
+
+print(d)
+
+
+# In[43]:
+
+
+s="pwskills"
+
+
+# In[44]:
+
+
+type(s)
+
+
+# In[45]:
+
+
+print(s)
+
+
+# In[46]:
+
+
+s[0]
+
+
+# In[47]:
+
+
+s[0:]
+
+
+# In[48]:
+
+
+s[::-1]
+
+
+# In[49]:
+
+
+s.lower()
+
+
+# In[50]:
+
+
+s.upper()
+
+
+# In[51]:
+
+
+s.capitalize()
+
+
+# In[52]:
+
+
+s.replace("p","g")
+
+
+# In[53]:
+
+
+s
+
+
+# In[54]:
+
+
+s.index("l")
+
+
+# In[64]:
+
+
+s.index("l",1,4)
+
+
+# In[62]:
+
+
+help(str)
+
+
+# In[68]:
+
+
+s.removeprefix("w")
+
+
+# In[72]:
+
+
+s.count("l",6)
+
+
+# In[79]:
+
+
+s.find("l",1,4)
+
+
+# In[82]:
+
+
+s.count("s")
+
+
+# In[83]:
+
+
+s[0:3]
+
+
+# In[84]:
+
+
+s[0::2]
+
+
+# In[85]:
+
+
+s[::-2]
+
+
+# In[86]:
+
+
+s[2:7:-1]
+
+
+# In[87]:
+
+
+s[7:0]
+
+
+# In[88]:
+
+
+s[7::-1]
+
+
+# In[89]:
+
+
+s[8:0:-1]
+
+
+# In[90]:
+
+
+s[9::-1]
+
+
+# In[91]:
+
+
+s[-2:-8:-1]
+
+
+# In[92]:
+
+
+s[90]
+
+
+# In[95]:
+
+
+s[:-90:-1]
+
+
+# In[96]:
+
+
+c=200
+
+
+# In[97]:
+
+
+type(c)
+
+
+# In[98]:
+
+
+c="200"
+
+
+# In[99]:
+
+
+c[-1]
+
+
+# In[100]:
+
+
+s1="this is my string class"
+
+
+# In[101]:
+
+
+s1
+
+
+# In[102]:
+
+
+s1[::-1]
+
+
+# In[103]:
+
+
+s1.count(" ")
+
+
+# In[105]:
+
+
+len(s1)
+
+
+# In[106]:
+
+
+s1
+
+
+# In[107]:
+
+
+s1.lower()
+
+
+# In[108]:
+
+
+s1.upper()
+
+
+# In[111]:
+
+
+s1.find("s")
+
+
+# In[112]:
+
+
+s.find("s")
+
+
+# In[113]:
+
+
+s1.find("i")
+
+
+# In[114]:
+
+
+s1.find("is")
+
+
+# In[115]:
+
+
+s1.find("iS")
+
+
+# In[117]:
+
+
+s1.isalnum()
+
+
+# In[121]:
+
+
+s1.center(3)
+
+
+# In[122]:
+
+
+s1.casefold()
+
+
+# In[123]:
+
+
+s1.count("s")
+
+
+# In[124]:
+
+
+s1.count("st")
+
+
+# In[125]:
+
+
+s1.count("z")
+
+
+# In[127]:
+
+
+s2=s1.upper()
+
+
+# In[128]:
+
+
+s2
+
+
+# In[129]:
+
+
+s2.lower()
+
+
+# In[130]:
+
+
+s1.title()
+
+
+# In[131]:
+
+
+s1.capitalize()
+
+
+# In[132]:
+
+
+s+1
+
+
+# In[133]:
+
+
+s+s1
+
+
+# In[134]:
+
+
+s*2
+
+
+# In[135]:
+
+
+s+"1"
+
+
+# In[136]:
+
+
+"don't do copy and apste in my class"
+
+
+# In[137]:
+
+
+'don't do copy and paste in my class'
+
+
+# In[141]:
+
+
+#THIS IS MY FIRST CLASS IN PROGRAMMING
+
+
+# In[142]:
+
+
+#this is my coding class
+
+
+# In[146]:
+
+
+""" i have learnt data type int,float, 
+boolean,string,NONE IT WAS GOD AND """
+
+
+# In[148]:
+
+
+""""This is my first class in programing
+i have learnt variable datatype,string,int, float,boolean,complex number
+i have seen some example of string slicing and indexing
+i have gone through some of string inbuilt function
+apart from that i am tryijng to practise in my system"""
+
+
+# In[ ]:
+
+
+# This is my first class in programing
+# i have learnt variable datatype,string,int, float,boolean,complex number
+# i have seen some example of string slicing and indexing
+# i have gone through some of string inbuilt function
+# apart from that i am tryijng to practise in my syste
+
+
+# In[149]:
+
+
+a=[21,3.1,True,False,"devvrat",None]
+
+
+# In[150]:
+
+
+a
+
+
+# In[151]:
+
+
+a[::-1]
+
+
+# In[152]:
+
+
+len(a)
+
+
+# In[153]:
+
+
+a[2::2]
+
+
+# In[154]:
+
+
+type(a)
+
+
+# In[155]:
+
+
+a.remove(True)
+
+
+# In[156]:
+
+
+a
+
+
+# In[157]:
+
+
+a.insert(2,"kela")
+
+
+# In[158]:
+
+
+a
+
+
+# In[159]:
+
+
+a.index(3.1)
+
+
+# In[161]:
+
+
+a.sort()
+
+
+# In[ ]:
+
+
+# sorts only when same data type is given
+
+
+# In[162]:
+
+
+b=[4,57,89,6,4,2,3,1,58,69]
+
+
+# In[163]:
+
+
+b.sort()
+
+
+# In[164]:
+
+
+b
+
+
+# In[165]:
+
+
+b.reverse()
+
+
+# In[166]:
+
+
+b
+
+
+# In[167]:
+
+
+a.reverse()
+
+
+# In[168]:
+
+
+a
+
+
+# In[169]:
+
+
+a.extend([1,2,34])
+
+
+# In[170]:
+
+
+a
+
+
+# In[171]:
+
+
+a.pop()
+
+
+# In[172]:
+
+
+a
+
+
+# In[173]:
+
+
+a[2::2]
+
+
+# In[174]:
+
+
+s="pswkills"
+
+
+# In[175]:
+
+
+a+s
+
+
+# In[176]:
+
+
+a.append(s)
+
+
+# In[177]:
+
+
+a
+
+
+# In[178]:
+
+
+a.extend(s)
+
+
+# In[179]:
+
+
+a
+
+
+# In[180]:
+
+
+a[1]
+
+
+# In[181]:
+
+
+a[1][0:2]
+
+
+# In[183]:
+
+
+l1=[3,4,5]
+
+
+# In[184]:
+
+
+a+l1
+
+
+# In[185]:
+
+
+l1*3
+
+
+# In[186]:
+
+
+len(a)
+
+
+# In[187]:
+
+
+l1
+
+
+# In[188]:
+
+
+l1.insert(1,[2,4,6])
+
+
+# In[189]:
+
+
+l1
+
+
+# In[190]:
+
+
+a.pop(4)
+
+
+# In[191]:
+
+
+a
+
+
+# In[193]:
+
+
+a.remove("p")
+
+
+# In[194]:
+
+
+a
+
+
+# In[195]:
+
+
+l1
+
+
+# In[196]:
+
+
+l1[1].pop(2)
+
+
+# In[197]:
+
+
+l1
+
+
+# In[199]:
+
+
+l1.insert(1,"sudh")
+
+
+# In[200]:
+
+
+l1
+
+
+# In[201]:
+
+
+l1[1].replace("dh","")
+
+
+# In[202]:
+
+
+l1
+
+
+# In[ ]:
+
+
+l1.sort()
+
+
+# In[204]:
+
+
+a=("a","b","c",1,2,3)
+
+
+# In[205]:
+
+
+a
+
+
+# In[206]:
+
+
+type(a)
+
+
+# In[207]:
+
+
+a.index("a")
+
+
+# In[210]:
+
+
+a.index(1)
+
+
+# In[211]:
+
+
+a.index(3)
+
+
+# In[212]:
+
+
+a.count(3)
+
+
+# In[213]:
+
+
+a=((1,2),(3,4,5),[4,5,6])
+
+
+# In[214]:
+
+
+a[2]
+
+
+# In[215]:
+
+
+a[2].pop()
+
+
+# In[216]:
+
+
+b=(3,4,8,9,2,3,4,6,7)
+
+
+# In[217]:
+
+
+sorted(b)
+
+
+# In[218]:
+
+
+len(b)
+
+
+# In[219]:
+
+
+b
+
+
+# In[220]:
+
+
+s={1,1,2,2,2,3,3,3,3,4,4,4,4}
+
+
+# In[221]:
+
+
+s
+
+
+# In[222]:
+
+
+s.add(5)
+
+
+# In[223]:
+
+
+s
+
+
+# In[224]:
+
+
+s.add([1,2,4])
+
+
+# In[226]:
+
+
+a=s.copy()
+
+
+# In[227]:
+
+
+a
+
+
+# In[228]:
+
+
+s
+
+
+# In[230]:
+
+
+s1={4,5,6,7,8,9}
+
+
+# In[231]:
+
+
+s&s1
+
+
+# In[232]:
+
+
+s
+
+
+# In[233]:
+
+
+s1
+
+
+# In[234]:
+
+
+s|s1
+
+
+# In[242]:
+
+
+l=["a",1,2,2,3,3,3,3]
+
+
+# In[243]:
+
+
+l
+
+
+# In[244]:
+
+
+set(l)
+
+
+# In[245]:
+
+
+s1[0]
+
+
+# In[246]:
+
+
+s1.remove(7)
+
+
+# In[247]:
+
+
+s1
+
+
+# In[248]:
+
+
+s1.pop()
+
+
+# In[249]:
+
+
+s1
+
+
+# In[251]:
+
+
+d={"name":"devvrat",
+   "lastname":"dubey",
+   "age":24,
+   "status":"unmarried"
+    
+}
+
+
+# In[252]:
+
+
+d
+
+
+# In[253]:
+
+
+d.keys()
+
+
+# In[254]:
+
+
+d.values()
+
+
+# In[255]:
+
+
+d.items()
+
+
+# In[257]:
+
+
+d["name"]
+
+
+# In[259]:
+
+
+d.get("names")
+
+
+# In[260]:
+
+
+d.get("name")
+
+
+# In[261]:
+
+
+d.pop("name")
+
+
+# In[262]:
+
+
+d
+
+
+# In[265]:
+
+
+d.popitem()
+
+
+# In[266]:
+
+
+d
+
+
+# In[267]:
+
+
+d["adress"]="rajbali chawl farid nagar"
+
+
+# In[268]:
+
+
+d
+
+
+# In[269]:
+
+
+a={}
+for i in range(100):
+    a[i]=i**2
+
+
+# In[270]:
+
+
+a
+
+
+# In[271]:
+
+
+for i in d:
+    print(i)
+
+
+# In[272]:
+
+
+"name"in d
+
+
+# In[273]:
+
+
+"age" in d
+
+
+# In[275]:
+
+
+for key,value in d.items():
+    print(f"key is {key} and its value is {value}")
+
+
+# In[279]:
+
+
+school= {
+        1:"dev",
+        2:"satu",
+        3:"bitto",
+        4:"sunita",
+        5:"anil"
+    }
+
+
+# In[280]:
+
+
+school[1]
+
+
+# In[282]:
+
+
+school[2]
+
+
+# In[283]:
+
+
+for key,value in school.items():
+    print(key)
+    print(value)
+
+
+# In[284]:
+
+
+d
+
+
+# In[285]:
+
+
+school
+
+
+# In[287]:
+
+
+school.update(d)
+
+
+# In[288]:
+
+
+school
+
+
+# In[289]:
+
+
+a
+
+
+# In[290]:
+
+
+d
+
+
+# In[292]:
+
+
+d.update(a)
+
+
+# In[293]:
+
+
+d
+
+
+# In[294]:
+
+
+d.update(school)
+
+
+# In[295]:
+
+
+d
+
+
+# In[299]:
+
+
+def studentmarks(marks):
+    if marks>80:
+        print("you are a bright student")
+    elif 60<marks<=80:
+        print("you are an average student")
+    else:
+        print("you are a below average student
+
+
+# In[301]:
+
+
+studentmarks(80)
+
+
+# In[302]:
+
+
+marks=60
+
+
+# In[303]:
+
+
+marks>80
+
+
+# In[304]:
+
+
+marks>40
+
+
+# In[305]:
+
+
+marks==60
+
+
+# In[312]:
+
+
+marks=int(input("Enter your marks"))
+if marks>80:
+    print("you are a bright student")
+elif 60<marks<=80:
+    print("you are an average student")
+else:
+    print("you are a below average student")
+
+
+# In[309]:
+
+
+type(marks)
+
+
+# In[315]:
+
+
+price=int(input("enter price"))
+if price>1000:
+    print("i will not buy")
+else:
+    print("i will buy")
+
+
+# In[316]:
+
+
+l=[1,2,3,4,5,6,7,8]
+
+
+# In[318]:
+
+
+a=[]
+for i  in l:
+    a.append(i+1)
+    
+
+
+# In[319]:
+
+
+a
+
+
+# In[320]:
+
+
+l=["sudh","kumar","pwskills","course"]
+
+
+# In[321]:
+
+
+l
+
+
+# In[322]:
+
+
+for i in l :
+    print(i)
+
+
+# In[326]:
+
+
+l1=[]
+for i in l:
+    l1.append(i.upper())
+
+
+# In[328]:
+
+
+l1
+
+
+# In[330]:
+
+
+l=[1,2,3,4,5,6,7.7,8,"sudh","Kumar","pwcourse"]
+
+
+# In[331]:
+
+
+l
+
+
+# In[338]:
+
+
+l1=[]
+l2=[]
+for i in l:
+    if type(i)==int or type(i)==float:
+        l1.append(i)
+    if type(i)==str:
+        l2.append(i)
+
+
+# In[339]:
+
+
+l1
+
+
+# In[340]:
+
+
+l2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
